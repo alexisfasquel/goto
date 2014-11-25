0be8575429bd32f1de6ea369de68b22db1cfbf30
fasquelito <alexis.fasquel@insa-lyon.fr>
2014-11-25 20:01:44 +0100

    changing method for redirection (way faster)

--- src/background.js
+++ src/background.js
@@ -6,40 +6,56 @@
  */
 
 
-var inProgress = false;
+var lastRequestId;
 
 // Authorizing the script when installing the extension
 chrome.runtime.onInstalled.addListener(function() {
 
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (!inProgress) {
-    for (var shortcut in localStorage) {
-      if (tab.url.substring(7, tab.url.length) == shortcut) {
-        inProgress = true;
-        chrome.tabs.update(tabId,
-                           {url: localStorage[shortcut]},
-                           function(tab) {
-                             inProgress = false;
-                           });
-      }
+//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//  if (!inProgress) {
+//    for (var shortcut in localStorage) {
+//      if (tab.url.substring(7, tab.url.length) == shortcut) {
+//        inProgress = true;
+//        chrome.tabs.update(tabId,
+//                           {url: localStorage[shortcut]},
+//                           function(tab) {
+//                             inProgress = false;
+//                           });
+//      }
+//    }
+//  }
+//});
+
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+  return goto(details);
+}, {
+  urls : ["<all_urls>"]
+}, ["blocking"]);
+
+function goto(details) {
+  for (var shortcut in localStorage) {
+    if(details.url == shortcut && details.requestId !== lastRequestId) {
+      lastRequestId = details.requestId;
+      return {
+        redirectUrl : localStorage[shortcut]
+      };
     }
   }
-});
+}
 
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action == "add") {
-      var short = request.shortcut + '/';
-
-      if(typeof localStorage[short] != 'undefined') {
-        sendResponse(true);
-      } else {
-        localStorage[short] = request.url;
-        sendResponse(false);
-      }
+  if (request.action == "add") {
+    var short = 'http://' + request.shortcut + '/';
+    if(typeof localStorage[short] != 'undefined') {
+      sendResponse(true);
+    } else {
+      localStorage[short] = request.url;
+      sendResponse(false);
     }
+  }
 });
 
 
