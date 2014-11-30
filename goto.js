1b2d1b0fcc81fcbf9d51ceac6d233d0fb1f89a0b
fasquelito <alexis.fasquel@insa-lyon.fr>
2014-11-30 20:44:50 +0100

    Final commit website - v0.0.2

---
+++ goto.js
@@ -0,0 +1,6 @@
+var buttons = document.getElementsByTagName('button');
+  for(var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    window.location = "https://chrome.google.com/webstore/category/apps";
+  })
+}
